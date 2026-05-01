0f14abb5923eb1af0ed1181963739b947d26f3ac
Roshan Reji <roshan.reji@experionglobal.local>
2026-05-01 16:58:09 +0530

    implemented changes in logger.ts util

--- framework/utils/logger.ts
+++ framework/utils/logger.ts
@@ -4,9 +4,21 @@ export const logger = winston.createLogger({
 
   level: 'info',
 
-  format: winston.format.simple(),
+  format: winston.format.combine(
+
+    winston.format.timestamp(),
+
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
 
   transports: [
-    new winston.transports.Console()
+
+    new winston.transports.Console(),
+
+    new winston.transports.File({
+      filename: 'logs/framework.log'
+    })
   ]
 });
