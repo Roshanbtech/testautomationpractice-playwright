cad7c2b1b452b531942184ee42680e2ad1e4ef87
Roshan Reji <roshan.reji@experionglobal.local>
2026-05-01 16:51:04 +0530

    global setup addon in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 import { BASE_URL } from './framework/config/env';
 
-
-dotenv.config();
+dotenv.config({ path: '.env' });
 
 export default defineConfig({
 
   testDir: './tests',
 
   fullyParallel: true,
+
   retries: 1,
+
   timeout: 30000,
+
+  globalSetup: require.resolve('./framework/setup/globalSetup'),
 
   reporter: [
     ['html'],
@@ -21,11 +24,15 @@ export default defineConfig({
   use: {
 
     baseURL: BASE_URL,
+
     browserName: 'chromium',
+
     headless: false,
-    
+
     screenshot: 'only-on-failure',
+
     video: 'retain-on-failure',
+
     trace: 'retain-on-failure'
   }
 });
